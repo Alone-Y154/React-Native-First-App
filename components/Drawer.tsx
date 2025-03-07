1dc6218e03dd73deba3ce4c2715c73d6c9ee5928
unknown <yashwanth.kotapati@vation.com>
2025-03-07 14:48:09 +0530

    nativeWind fix for other paths

--- components/Drawer.tsx
+++ components/Drawer.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import TodoScreen from '@/screens/TodoScreen';
 import ProfileScreen from '@/screens/ProfileScreen';
+import "../global.css";
 
 const Drawer = createDrawerNavigator();
 
 export default function MyDrawer() {
   return (
     <Drawer.Navigator>
-      <Drawer.Screen name="To do List" component={TodoScreen} />
-      <Drawer.Screen name="Profile" component={ProfileScreen} />
+      <Drawer.Screen name="Todo List" component={TodoScreen} />
+      <Drawer.Screen name="Edo new functionality" component={ProfileScreen} />
       {/* Add more screens as needed */}
     </Drawer.Navigator>
   );
--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import "../global.css";
 
 const ProfileScreen = () => {
   return (
-    <View className='flex flex-row outline-dashed justify-center items-center'>
-      <Text>Profile Screen</Text>
+    <View className='flex flex-row outline-2 border outline-dashed justify-center items-center'>
+      <Text className='text-lg font-bold'>Profile Screen</Text>
       <Text>Heellooooooo</Text>
     </View>
   );
